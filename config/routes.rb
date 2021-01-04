2863d859af51ef39a8018e531331cdab347dc4fa
Abimanyu007 <rsabimanyu@mail.com>
2021-01-05 00:29:54 +0530

    Create rails app and setup home and about pages

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'pages#home'
+  get '/about', to: 'pages#about'
+  get 'welcome/home', to: 'welcome#home'
+  get 'welcome/about', to: 'welcome#about'
+end
